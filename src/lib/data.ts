c5175bc31abdf206a40a00ef1056b68b569e3f36
Vilas Chauvhan <chavhanvilas02@gmail.com>
2025-10-06 21:45:08 +0530

    fix: created static skills data

---
+++ src/lib/data.ts
@@ -0,0 +1,3 @@
+export const FRONTEND = ["React", "Tailwind", "Nextjs", "Redux toolkit", "Zustand", "Tanstack Eco System", "Shadcn", "Figma"];
+export const BACKEND = ["Nodejs", "Expressjs", "TypeORM"];
+export const SERVICES = ["AWS", "MongoDB", "Postgress", "Postman", "Docker"]
