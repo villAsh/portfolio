38d7f5040585091b65ae2cee2f0b734ca06a1994
Vilas Chauvhan <chavhanvilas02@gmail.com>
2025-10-08 22:21:45 +0530

    fix: updated social icons

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -104,38 +104,37 @@ export const Gmail = () => {
       className={"p-2 border rounded-[12px] hover:cursor-pointer"}
     >
       <motion.path
+        className={"fill-black dark:fill-white"}
         variants={pathVariants}
         initial="hidden"
         animate="visible"
-        fill="#4caf50"
         d="M45,16.2l-5,2.75l-5,4.75L35,40h7c1.657,0,3-1.343,3-3V16.2z"
       />
       <motion.path
+        className={"fill-black dark:fill-white"}
         variants={pathVariants}
         initial="hidden"
         animate="visible"
-        fill="#1e88e5"
         d="M3,16.2l3.614,1.71L13,23.7V40H6c-1.657,0-3-1.343-3-3V16.2z"
       />
       <motion.polygon
         variants={pathVariants}
+        className={"fill-black dark:fill-white"}
         initial="hidden"
         animate="visible"
-        fill="#e53935"
         points="35,11.2 24,19.45 13,11.2 12,17 13,23.7 24,31.95 35,23.7 36,17"
       />
       <motion.path
+        className={"fill-black dark:fill-white"}
         variants={pathVariants}
         initial="hidden"
-        animate="visible"
-        fill="#c62828"
         d="M3,12.298V16.2l10,7.5V11.2L9.876,8.859C9.132,8.301,8.228,8,7.298,8h0C4.924,8,3,9.924,3,12.298z"
       />
       <motion.path
+        className={"fill-black dark:fill-white"}
         variants={pathVariants}
         initial="hidden"
         animate="visible"
-        fill="#fbc02d"
         d="M45,12.298V16.2l-10,7.5V11.2l3.124-2.341C38.868,8.301,39.772,8,40.702,8h0 C43.076,8,45,9.924,45,12.298z"
       />
     </motion.svg>
@@ -153,7 +152,7 @@ export const LinkedIn = () => {
       variants={svgVariants}
       initial="hidden"
       animate="visible"
-      className={"p-2 border rounded-[12px] hover:cursor-pointer"}
+      className={"p-2.5 border rounded-[12px] hover:cursor-pointer"}
     >
       <motion.g clipPath="url(#clip0_17_32)">
         <motion.path
@@ -161,7 +160,7 @@ export const LinkedIn = () => {
           initial="hidden"
           animate="visible"
           d="M44.4567 0H3.54333C2.60358 0 1.70232 0.373315 1.03782 1.03782C0.373315 1.70232 0 2.60358 0 3.54333V44.4567C0 45.3964 0.373315 46.2977 1.03782 46.9622C1.70232 47.6267 2.60358 48 3.54333 48H44.4567C45.3964 48 46.2977 47.6267 46.9622 46.9622C47.6267 46.2977 48 45.3964 48 44.4567V3.54333C48 2.60358 47.6267 1.70232 46.9622 1.03782C46.2977 0.373315 45.3964 0 44.4567 0ZM14.3067 40.89H7.09V17.9667H14.3067V40.89ZM10.6933 14.79C9.87473 14.7854 9.07583 14.5384 8.39747 14.0802C7.71911 13.622 7.19168 12.9731 6.88175 12.2154C6.57183 11.4577 6.4933 10.6252 6.65606 9.82291C6.81883 9.02063 7.2156 8.28455 7.79631 7.70756C8.37702 7.13057 9.11563 6.73853 9.91893 6.58092C10.7222 6.42331 11.5542 6.50719 12.3099 6.82197C13.0656 7.13675 13.7111 7.66833 14.1649 8.34962C14.6188 9.03092 14.8606 9.83138 14.86 10.65C14.8677 11.1981 14.765 11.7421 14.558 12.2496C14.351 12.7571 14.044 13.2178 13.6551 13.6041C13.2663 13.9905 12.8037 14.2946 12.2948 14.4983C11.786 14.702 11.2413 14.8012 10.6933 14.79ZM40.9067 40.91H33.6933V28.3867C33.6933 24.6933 32.1233 23.5533 30.0967 23.5533C27.9567 23.5533 25.8567 25.1667 25.8567 28.48V40.91H18.64V17.9833H25.58V21.16H25.6733C26.37 19.75 28.81 17.34 32.5333 17.34C36.56 17.34 40.91 19.73 40.91 26.73L40.9067 40.91Z"
-          className={"fill-[#0A66C2] dark:fill-white"}
+          className={"fill-black dark:fill-white"}
         />
       </motion.g>
       <defs>
