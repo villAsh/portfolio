38f14527d8bf409e0b620980e2ccd86859a4c617
Vilas Chauvhan <chavhanvilas02@gmail.com>
2025-10-09 21:59:02 +0530

    fix: updated frontend data position

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,3 @@
-export const FRONTEND = ["React", "Tailwind", "Nextjs", "Redux toolkit", "Zustand", "Tanstack Eco System", "Shadcn", "Figma"];
+export const FRONTEND = ["React", "Tailwind", "Nextjs", "Redux toolkit", "Zustand","Figma", "Tanstack Eco System", "Shadcn"];
 export const BACKEND = ["Nodejs", "Expressjs", "TypeORM"];
 export const SERVICES = ["AWS", "MongoDB", "Postgress", "Postman", "Docker"]
