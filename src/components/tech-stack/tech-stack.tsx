220ded983ec80dcaa5a60cfd137fcadb783e3870
Vilas Chauvhan <chavhanvilas02@gmail.com>
2025-10-10 22:07:33 +0530

    fix: updated techk stack and remove the heading

--- src/components/tech-stack/tech-stack.tsx
+++ src/components/tech-stack/tech-stack.tsx
@@ -18,23 +18,7 @@ const skillContainer = {
 const TechStack = () => {
   return (
     <AnimatePresence>
-      <motion.div
-        layout
-        className="border-4 w-full px-4 py-2 rounded-xl h-full p-1 relative group"
-        whileHover={"hover"}
-      >
-        <motion.div className="flex flex-row items-center justify-center gap-y-4">
-          <LeftCurlyBrace />
-          <motion.h1
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.6, delay: 0.8 }}
-            className="text-3xl font-bold uppercase bg-clip-text text-transparent bg-gradient-to-b from-gray-800 to-80% to-gray-300 dark:from-gray-100 dark:via-gray-300 dark:via-55% dark:to-gray-500"
-          >
-            SKILLS
-          </motion.h1>
-          <RightCurlyBrace />
-        </motion.div>
+      <motion.div className="border-4 w-full px-4 rounded-xl h-full relative group">
         <motion.div
           variants={skillContainer}
           initial="hidden"
