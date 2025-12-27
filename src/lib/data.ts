8f0b66514797f3720df1be807ed710d9d364d40a
Vilas Chauvhan <chavhanvilas02@gmail.com>
2025-12-27 11:15:46 +0530

    fix: updated skills data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,14 @@
-export const FRONTEND = ["React", "Tailwind", "NextJs", "Redux toolkit", "Zustand","Figma", "Tanstack Eco System", "Shadcn"];
+export const FRONTEND = [
+  "React",
+  "React Native",
+  "Expo",
+  "Tailwind",
+  "NextJs",
+  "Redux toolkit",
+  "Zustand",
+  "Figma",
+  "Tanstack Eco System",
+  "Shadcn",
+];
 export const BACKEND = ["NodeJs", "ExpressJs", "TypeORM"];
-export const SERVICES = ["AWS", "MongoDB", "Postgress", "Postman", "Docker"]
+export const SERVICES = ["AWS", "MongoDB", "Postgress", "Postman", "Docker"];
