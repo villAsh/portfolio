11504836f56e4535ad83083648b1ef38f5e57c23
Vilas Chauvhan <chavhanvilas02@gmail.com>
2026-01-10 11:16:39 +0530

    fix: added tech stack section

---
+++ src/components/sections/tech-stack.tsx
@@ -0,0 +1,11 @@
+const TechStack = () => {
+  return (
+    <section className="mt-10 max-w-2xl mx-auto w-full">
+      <h1 className="text-2xl font-semibold text-neutral-900 dark:text-neutral-200">
+        Tech Stack
+      </h1>
+    </section>
+  );
+};
+
+export default TechStack;
