ae252847756f5c994491b8505bd162cbef7c35d8
Vilas Chauvhan <chavhanvilas02@gmail.com>
2026-01-10 11:06:56 +0530

    fix: added about me in the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import BottomFade from "@/components/bottom-fade/bottom-fade";
+import AboutMe from "@/components/sections/about-me";
 import Experience from "@/components/sections/experience";
 import HeroSection from "@/components/sections/hero";
+import Projects from "@/components/sections/projects";
 
 export default function Home() {
   return (
-    <main className="w-screen mx-auto max-md:px-10 relative">
+    <main className="w-screen mx-auto max-md:px-10 relative pb-10">
       <HeroSection />
       <Experience />
+      <Projects />
+      <AboutMe />
       <BottomFade />
     </main>
   );
