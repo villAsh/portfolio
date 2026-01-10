1818ec5d133e8f23c74b6051590fbe560fe2b6bf
vilas chauvhan <chavhanvilas02@gmail.com>
2026-01-10 13:13:02 +0530

    font: include grotesk font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,19 @@ import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import Navbar from "@/components/navbar";
 
+import { Schibsted_Grotesk } from "next/font/google";
+
 export const metadata: Metadata = {
   title: "Vilas - a Software Engineer.",
   description: "Home page of Vilas Chauvhan Portfolio",
 };
+
+const grotesk = Schibsted_Grotesk({
+  variable: "--grotesk",
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "600", "700"],
+});
 
 export default function RootLayout({
   children,
@@ -14,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`antialiased transition-colors duration-500`}>
+    <html lang="en" suppressHydrationWarning className={grotesk.variable}>
+      <body
+        className={`antialiased transition-colors duration-500 ${grotesk.className}`}
+      >
         <ThemeProvider
           defaultTheme="light"
           attribute={"class"}
