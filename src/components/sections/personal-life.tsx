4df0b7eee8698626e4d8961eccdb88331b6a5f97
vilas chauvhan <chavhanvilas02@gmail.com>
2026-01-13 21:17:39 +0530

    fix: created card reusable component

--- src/components/sections/personal-life.tsx
+++ src/components/sections/personal-life.tsx
@@ -1,8 +1,30 @@
 "use client";
 import { INITIAL, TRANSITION, WHILE_IN_VIEW } from "@/lib/framerConfig";
-import { Film, LibraryBig } from "lucide-react";
+import { Film, LibraryBig, type LucideIcon } from "lucide-react";
 import { motion } from "motion/react";
 
+type TLifeCard = {
+  Icon: LucideIcon;
+  title: string;
+  subTitle: string;
+};
+const LifeCard = ({ Icon, title, subTitle }: TLifeCard) => {
+  return (
+    <div className="flex flex-row items-center justify-start gap-x-2 border p-3 rounded-[10px] inset-shadow-sm hover:inset-shadow-muted cursor-pointer">
+      <div className="p-1.5 bg-neutral-200 rounded-[8px]">
+        <Icon size={24} className="stroke-neutral-800 stroke-[1.2]" />
+      </div>
+      <div className="flex flex-col items-start gap-y-px">
+        <h1 className="text-base font-medium text-neutral-800 dark:text-neutral-200">
+          {title}
+        </h1>
+        <span className="text-xs tracking-wide text-neutral-500 font-medium dark:text-neutral-400">
+          {subTitle}
+        </span>
+      </div>
+    </div>
+  );
+};
 const PersonalLife = () => {
   return (
     <section className="mt-32 max-w-2xl mx-auto w-full mb-20">
@@ -23,35 +45,16 @@ const PersonalLife = () => {
           Life
         </h2>
         <div className="flex flex-col gap-y-4">
-          <div className="flex flex-row items-center justify-start gap-x-2">
-            <div className="p-1.5 bg-neutral-400 rounded-[8px]">
-              <Film size={24} className="stroke-neutral-800 stroke-[1.2]" />
-            </div>
-            <div className="flex flex-col items-start gap-y-px">
-              <h1 className="text-base font-medium text-neutral-800 dark:text-neutral-200">
-                Movies & Show
-              </h1>
-              <span className="text-xs tracking-wide text-neutral-500 font-medium dark:text-neutral-400">
-                Movies and shows that have inspired me{" "}
-              </span>
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-start gap-x-2">
-            <div className="p-1.5 bg-neutral-400 rounded-[8px]">
-              <LibraryBig
-                size={24}
-                className="stroke-neutral-800 stroke-[1.2]"
-              />
-            </div>
-            <div className="flex flex-col items-start gap-y-px">
-              <h1 className="text-base font-medium text-neutral-800 dark:text-neutral-200">
-                Books
-              </h1>
-              <span className="text-xs tracking-wide text-neutral-500 font-medium dark:text-neutral-400">
-                Books that have influenced my thinking and personal growth
-              </span>
-            </div>
-          </div>
+          <LifeCard
+            Icon={Film}
+            title="Movies & Show"
+            subTitle="Movies and shows that have inspired me"
+          />
+          <LifeCard
+            Icon={LibraryBig}
+            title="Books"
+            subTitle="Books that have influenced my thinking and personal growth"
+          />
         </div>
       </motion.div>
     </section>
