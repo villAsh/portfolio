1d6495937ab52b48a5885ad691752e205d5cf20d
vilas chauvhan <chavhanvilas02@gmail.com>
2026-04-16 22:08:25 +0530

    fix: updated oneko to make it visible in only home page

--- src/components/oneko/oneko.tsx
+++ src/components/oneko/oneko.tsx
@@ -1,6 +1,33 @@
+"use client";
+
 import Script from "next/script";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Oneko = () => {
+  const pathname = usePathname();
+  const [isHome, setIsHome] = useState(false);
+
+  useEffect(() => {
+    setIsHome(pathname === "/");
+    if (pathname !== "/") {
+      const cat = document.getElementById("oneko");
+      if (cat) {
+        cat.remove();
+      }
+    }else{
+      const cat = document.getElementById("oneko");
+      if (!cat) {
+        const script = document.createElement("script");
+        script.src = "/oneko/oneko.js";
+        script.dataset.cat = "/oneko/oneko.gif";
+        document.body.appendChild(script);
+      }
+    }
+  }, [pathname]);
+
+  if (!isHome) return null;
+
   return <Script src="/oneko/oneko.js" data-cat="/oneko/oneko.gif" />;
 };
 
